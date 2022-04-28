073fb455ffea66a4bc71ffb0660872cc22e4cf52
ideook <ideook@gmail.com>
2022-04-28 10:10:39 +0900

    up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { FormIt } from "https://formit3d.github.io/SharedPluginUtilities/FormIt.mod.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: null,
+      fileName: "",
+    };
+    this.handleFileChange = this.handleFileChange.bind(this);
+  }
+
+  handleFileChange(e) {
+    this.setState({
+      file: e.target.files[0],
+      fileName: e.target.value,
+    });
+    console.log(e.target.files[0]);
+
+    console.log("openSketch(): start", e.target.value);
+    FormIt.OpenFile(e.target.value);
+    console.log("openSketch(): end: ", e.target.value);
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          accept="model/x-formit, .axm"
+          id="file-axm"
+          type="file"
+          file={this.state.file}
+          value={this.state.fileName}
+          onChange={this.handleFileChange}
+        />
+        Select FormIt File
+      </div>
+    );
+  }
 }
 
 export default App;
